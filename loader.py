9e230cf5458472a1a6edc38a528bf0f20c0f9eb1
ldmcdona <ldmcdona@ualberta.ca>
2018-11-24 14:41:28 -0700

    phase 2 skeleton

---
+++ loader.py
@@ -0,0 +1,32 @@
+import sys
+import fileinput
+import re
+import pdb
+import os
+
+class Tree:
+    root = []
+    leaf = [[]]
+
+    def insert(self, key, data):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def search(self, key):
+        #insert code here
+        return data
+
+class Hash:
+    table = {}
+
+    def insert(self, key, data):
+        pass
+
+    def delete(self, key):
+        del table[key]
+
+    def search(self, key):
+        #insert code here
+        return data
