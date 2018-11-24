a3201be767364d0f91132065cf1e92d3d766114d
ldmcdona <ldmcdona@ualberta.ca>
2018-11-24 14:26:58 -0700

    skeleton for B+-tree

---
+++ loader.py
@@ -0,0 +1,15 @@
+import sys
+import fileinput
+import re
+import pdb
+import os
+
+class Tree:
+    root = []
+    leaf = [[]]
+
+    def insert(self, data):
+        pass
+
+    def delete(self, data):
+        pass
