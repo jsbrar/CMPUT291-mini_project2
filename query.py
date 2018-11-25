f7869e7498a8e230330556f19461e44488b8c510
ldmcdona <ldmcdona@ualberta.ca>
2018-11-25 14:19:44 -0700

    5% of phase 3

--- query.py
+++ query.py
@@ -4,19 +4,25 @@ def query():
     mess = []
     quest = input("Enter valid query: ")
     for char in range(len(quest)):
-        if quest[char] != '<' or quest[char] != '>' or quest[char] != '=':
+        if quest[char] != '<' and quest[char] != '>' and quest[char] != '=':
+            #print("flag0", quest[char])
             mess.append(quest[char])
             check = 0
         else:
+            #print("flag1", quest[char])
             if quest[char] == '=' and check == 0:
                 mess.append(quest[char])
-            else:
-            check = 1
+            elif quest[char] == '<' or quest[char] == '>':
+                if quest[char+1] == '=':
+                    check = 1
+                    if quest[char] == '<':
+                        mess.append('<=')
+                    elif quest[char] == '>':
+                        mess.append('>=')
+                else:
+                    mess.append(quest[char])
             
-        #print(mess) 
-    
-    for char in range(len(mess)):
-        if mess[char] == '<'
+    print(mess) 
       
 
 def main():
