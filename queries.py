7377b2fdea548541891d4f133ebf47187876d86a
navjit146 <43222013+navjit146@users.noreply.github.com>
2018-11-26 10:02:02 -0700

    use after pr2.py, query 1 done

--- queries.py
+++ queries.py
@@ -14,14 +14,27 @@ def query1():
     iter = curs.set_range("camera".encode("utf-8")) 
     ad_ids_list = []
     while str(iter[0].decode("utf-8")) == "camera":
-            print("term: " + str(iter[0].decode("utf-8")) + ", ad id : " + str(iter[1].decode("utf-8")))
-            ad_ids_list.append(str(iter[1].decode("utf-8")))    
+            ad_ids_list.append(str(iter[1].decode("utf-8")))  
             iter = curs.next()
+    #print(ad_ids_list)
+    
+    database.close()
+    database = db.DB()
+    DB_File = "ads.idx"
+    database.open(DB_File, None, db.DB_HASH)
+    curs = database.cursor()    
+    for ad in ad_ids_list:
+        iter = curs.set_range(ad.encode("utf-8"))
+        while str(iter[0].decode("utf-8")) == ad:
+            print("Record: " + str(iter[1].decode("utf-8")))  
+            print(" ")
+            iter = curs.next()   
+    
 
 
 
 def main():
     print("------------------------------")
-    query1()
+    #query1()
   
 main()
