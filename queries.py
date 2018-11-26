581638a35eb1e23f2c7befd79445227848dd06a1
navjit146 <43222013+navjit146@users.noreply.github.com>
2018-11-26 09:47:03 -0700

    separate queries and fixed

---
+++ queries.py
@@ -0,0 +1,28 @@
+import sys
+import fileinput
+import re
+import pdb
+import os
+from bsddb3 import db
+
+def query1():
+    database = db.DB()
+    DB_File = "terms.idx"
+    database.open(DB_File, None, db.DB_BTREE)
+    curs = database.cursor()
+
+    iter = curs.set_range("camera".encode("utf-8")) 
+    ad_ids_list = []
+    while str(iter[0].decode("utf-8")) == "camera":
+        if str(iter[0].decode("utf-8")) == "camera":
+            print("term: " + str(iter[0].decode("utf-8")) + ", ad id : " + str(iter[1].decode("utf-8")))
+            ad_ids_list.append(str(iter[1].decode("utf-8")))    
+            iter = curs.next()
+        print(iter)
+
+
+def main():
+    print("------------------------------")
+    query1()
+  
+main()
