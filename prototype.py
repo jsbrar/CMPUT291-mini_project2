743d0931efae647b3ac45b07d78853bf7a2a3c7c
ldmcdona <ldmcdona@ualberta.ca>
2018-11-26 13:50:48 -0700

    queries 7 and 8 are finalized

---
+++ prototype.py
@@ -0,0 +1,215 @@
+import os
+from bsddb3 import db
+
+def query4(oper, value, pile=[]):
+    #print("flag 0 ", oper, value, pile)
+    new_pile = []
+    key = ["<=", ">=", "=", "<", ">"].index(oper)
+    date = value.split("/")
+    for item in range(len(date)):
+        date[item] = int(date[item])
+    database = db.DB()
+    dafile = "pdates.idx"
+    database.open(dafile, None, db.DB_BTREE)
+    curs = database.cursor()
+    
+    """
+    if oper == "<=":
+        iter = curse.set_range(value.encode("utf-8"), value.encode("utf-8"))
+    elif oper == ">=":
+        iter = curse.set_range(value.encode("utf-8"), None,  value.encode("utf-8"))
+    elif oper == "=":
+        iter = curs.set_range(value.encode("utf-8"))
+    elif oper == "<":
+        fixed = value.split("/")
+        fixed[2] = str(int(fix[2] + 1))
+        fixed = '/'.join(fixed)
+        iter = curs.set_range(value.encode("utf-8"), fixed.encode("utf-8"))
+    elif oper == ">":
+        fixed = value.split("/")
+        fixed[2] = str(int(fix[2] - 1))
+        fixed = '/'.join(fixed)
+        iter = curs.set_range(value.encode("utf-8"), None, fixed.encode("utf-8"))
+        
+    """
+
+    iter = curs.first()
+    #print(iter[0].decode("utf-8"))
+    #print(iter[1].decode("utf-8"))
+    #print(len(pile))
+
+    if len(pile) != 0:
+        for item in pile:
+            while iter != None:
+                aid = iter[1].decode("utf-8")
+                temp = iter[0].decode("utf-8").split("/")
+                for item in range(len(temp)):
+                    temp[item] = int(temp[item])
+                if ((temp[0] < date[0]) or (temp[0] == date[0] and temp[1] < date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] <= date[2])) and key == 0 and item == aid: 
+                    new_pile.append(aid) 
+                elif ((temp[0] > date[0]) or (temp[0] == date[0] and temp[1] > date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] >= date[2])) and key == 1 and item == aid:
+                    new_pile.append(aid)
+                elif temp == date and key == 2 and item == aid:
+                    new_pile.append(aid)
+                elif ((temp[0] < date[0]) or (temp[0] == date[0] and temp[1] < date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] < date[2])) and key == 3 and item == aid:
+                    new_pile.append(aid)
+                elif ((temp[0] > date[0]) or (temp[0] == date[0] and temp[1] > date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] > date[2])) and key == 4 and item == aid:
+                    new_pile.append(aid)
+                iter = curs.next()
+    else:
+        while iter != None:
+            aid = iter[1].decode("utf-8")
+            aid = aid.split(",")
+            temp = iter[0].decode("utf-8").split("/")
+            for item in range(len(temp)):
+                temp[item] = int(temp[item])
+            
+            #print(temp, " vs ", date)
+
+            if ((temp[0] < date[0]) or (temp[0] == date[0] and temp[1] < date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] <= date[2])) and key == 0: 
+                new_pile.append(aid[0])
+            elif ((temp[0] > date[0]) or (temp[0] == date[0] and temp[1] > date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] >= date[2])) and key == 1:
+                new_pile.append(aid[0])
+            elif temp == date and key == 2:
+                new_pile.append(aid[0])
+            elif ((temp[0] < date[0]) or (temp[0] == date[0] and temp[1] < date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] < date[2])) and key == 3:
+                new_pile.append(aid[0])
+            elif ((temp[0] > date[0]) or (temp[0] == date[0] and temp[1] > date[1]) or (temp[0] == date[0] and temp[1] == date[1] and temp[2] > date[2])) and key == 4:
+                new_pile.append(aid[0])
+            iter = curs.next()
+
+    database.close()
+    return new_pile
+
+def query6(oper, value, pile=[]):
+    new_pile = []
+    key = ["<=", ">=", "=", "<", ">"].index(oper)
+    database = db.DB()
+    dafile = "price.idx"
+    database.open(dafile, None, db.BD_BTREE)
+    curs = database.cursor()
+
+    iter = curs.set_range(value.encode("utf-8"))
+
+    if len(pile) != 0:
+        for item in pile:
+            while iter != None:
+                aid = iter[1].decode("utf-8")
+                if int(iter[0].decode("utf-8")) <= value and key == 0 and aid == item:
+                    #new_pile.append(aid)
+                    pass
+                elif int(iter[0].decode("utf-8")) >= value and key == 1 and aid == item:
+                    pass
+                elif int(iter[0].decode("utf-8")) == value and key == 2 and aid == item:
+                    pass
+                elif int(iter[0].decode("utf-8")) < value and key == 3 and aid == item:
+                    pass
+                elif int(iter[0].decode("utf-8")) > value and key == 4 and aid == item:
+                    pass
+                iter = curs.next()
+    else:
+        while iter != None:
+            aid = iter[1].decode("utf-8")
+            if int(iter[0].decode("utf-8")) <= value and key == 0:
+                #new_pile.append(aid)
+                pass
+            elif int(iter[0].decode("utf-8")) >= value and key == 1:
+                pass
+            elif int(iter[0].decode("utf-8")) == value and key == 2:
+                pass
+            elif int(iter[0].decode("utf-8")) < value and key == 3:
+                pass
+            elif int(iter[0].decode("utf-8")) > value and key == 4:
+                pass
+            iter = curs.next()
+    
+    database.close()
+    return new_pile
+
+def query7(loc, pile=[]):
+    new_pile = []
+    database = db.DB()
+    dafile = "pdates.idx"
+    database.open(dafile, None, db.DB_BTREE)
+    curs = database.cursor()
+
+    iter = curs.first()
+
+    if len(pile) != 0:
+        for item in pile:
+            while iter != None:
+                aid = iter[1].decode("utf-8")
+                aid = aid.split(",")
+
+                if str(aid[2]) == loc and aid[0] == item:
+                    new_pile.append(aid[0])
+                iter = curs.next()
+    else:
+        while iter != None:
+            aid = iter[1].decode("utf-8")
+            aid = aid.split(",")
+            
+            if str(aid[2]) == loc:
+                new_pile.append(aid[0])
+            iter = curs.next()
+
+    database.close()
+    return new_pile
+
+def query8(cat, pile=[]):
+    new_pile = []
+    database = db.DB()
+    dafile = "pdates.idx"
+    database.open(dafile, None, db.DB_BTREE)
+    curs = database.cursor()
+
+    iter = curs.first()
+
+    if len(pile) != 0:
+        for item in pile:
+            while iter != None:
+                aid = iter[1].decode("utf-8")
+                aid = aid.split(",")
+                if aid[1] == cat and aid[0] == item:
+                    new_pile.append(aid[0])
+                iter = curs.next()
+    else:
+        while iter != None:
+            aid = iter[1].decode("utf-8")
+            aid = aid.split(",")
+            if aid[1] == cat:
+                new_pile.append(aid[0])
+            iter = curs.next()
+
+    database.close()
+    return new_pile
+    
+
+def final(pile, setting):
+    answer = []
+    if setting == "brief":
+        titles = open("titles.txt", "r")
+        for item in pile:
+            for title in titles:
+                test = title.split(":")
+                if test[1] == item:
+                    answer.append(test[0])
+    elif setting == "full":
+        ads = open(ads.txt, "r")
+        for item in pile:
+            for ad in ads:
+                test = ad.split(":")
+                if test[0] == item:
+                    answer.append(test[1])
+    return answer
+
+def main():
+    pile = []
+    pile = query4("=", "2018/11/7", pile)
+    print(pile)
+    print(len(pile))
+    pile = query8("art-collectibles", pile)
+    print(pile)
+    print(len(pile))
+    
+main()
